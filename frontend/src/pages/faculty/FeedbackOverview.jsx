f23ce436b2c1fdb8029d3431e5540a9f4848e894
Vaibhav Vaid <142966207+vaibhavvaid14@users.noreply.github.com>
2026-03-11 00:24:42 +0530

    Added FeedbackOverview component,jsx

---
+++ frontend/src/pages/faculty/FeedbackOverview.jsx
@@ -0,0 +1,11 @@
+import CourseFeedback from "../../components/faculty/CourseFeedback";
+
+const FeedbackOverview = () => {
+  return (
+    <div className="p-6">
+      <CourseFeedback courseId="CS101" />
+    </div>
+  );
+};
+
+export default FeedbackOverview;
